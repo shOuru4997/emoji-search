ad266a76c397080f8b6165c4206061b7b5bb1042
shOuru4997 <souravkumar4997@gmail.com>
2021-10-15 18:59:58 +0530

    enter

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import './Header.css'
+
+function Header() {
+  
+  return (
+    <div className="header">
+      <h1>
+        Emoji Search
+      </h1>
+      <p>
+        A simple emoji search tool made with ReactJS.
+      </p>
+    </div>
+  )
+}
+
+export default Header
